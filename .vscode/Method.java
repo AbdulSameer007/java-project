6e9df185968fcd4faad57964051d547714e98ca0
Raja <devgems007@gmail.com>
2022-02-02 19:35:00 +0530

    use of new keyward

---
+++ .vscode/Method.java
@@ -0,0 +1,13 @@
+public class Method {
+    private void name() {
+        System.out.println("my name is a raja");
+    }
+     private void age(){
+         System.out.println(12);
+     }
+     public static void main(String[] args) {
+        Method obj = new Method();
+        obj.name();
+        obj.age();
+     }
+}
